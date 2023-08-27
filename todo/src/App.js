d261def29341cc5c16dc9c5614a358ee4d327f52
Hayk1515 <kostanyan.hayk@gmail.com>
2023-08-27 15:15:52 +0400

    full

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        { name: "Cristiano", surname: "Ronaldo", age: 38, id: 1 },
+        { name: "Elon", surname: "Musk", age: 52, id: 2 },
+        { name: "Mark", surname: "Zuckerberg", age: 39, id: 3 },
+        { name: "Jeff", surname: "Bezos", age: 59, id: 4 },
+        { name: "Bill", surname: "Gates", age: 68, id: 5 },
+      ],
+      searchName: "",
+      searchSurname: "",
+    }
+    this.userRef = React.createRef('')
+    this.userRefSur = React.createRef('')
+  }
+
+  changeName = (event) => {
+    this.setState({
+      searchName: event.target.value
+    })
+  }
+  changeSurname = (ev) => {
+    this.setState({
+      searchSurname: ev.target.value
+    })
+  }
+
+  filterUsers = (item) => {
+    return item.name.toLowerCase().includes(this.state.searchName.toLowerCase())
+  }
+  filterUserS = (item) => {
+    return item.surname.toLowerCase().includes(this.state.searchSurname.toLowerCase())
+  }
+
+  deleteUser = (id) => {
+    let filteredArray = this.state.users.filter(el => el.id !== id)
+    this.setState({
+      users: filteredArray
+    })
+  }
+
+  addUser = () => {
+    let newUsers = {
+      name: this.userRef.current.value,
+      surname: this.userRefSur.current.value,
+      id: this.state.newUserID
+    }
+    this.setState({
+      users: [...this.state.users, ...[newUsers]],
+      newUserId: this.state.newUserId + 1
+    })
+    this.userRef.current.value = ''
+    this.userRefSur.current.value = ''
+  }
+
+  render() {
+    return (
+      <section>
+        <div className="input-box">
+
+          <div className="search-box">
+            <span>Search Users</span>
+            <input placeholder="Search Name" onChange={this.changeName} /> <br />
+            <input placeholder="Search Surname" onChange={this.changeSurname} />
+
+          </div>
+
+          <br />
+
+          <div className="create-box">
+            <span>Create New Users</span>
+
+            <input placeholder="New user name" ref={this.userRef} /> <br />
+            <input placeholder="New user surname" ref={this.userRefSur} />
+            <button onClick={this.addUser} >Add User</button>
+
+          </div>
+
+
+
+        </div>
+
+
+        <div className="list-box">
+          <ol>
+            {this.state.users.filter(this.filterUsers).filter(this.filterUserS).map(user => {
+              return (
+                <li>
+
+                  <span>Name:{user.name}</span>
+                  <br />
+                  <span>Surname:{user.surname}</span>
+                  <br />
+                  <button onClick={() => this.deleteUser(user.id)}>Delete user</button>
+
+                </li>
+              )
+            })}
+          </ol>
+        </div>
+      </section>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
+
